c755ceca03a23ea553fef113b1084c4c5e3d287d
Niioka <ryou3018@gmail.com>
2019-01-10 11:04:37 +0900

    読み込み方法の変更

--- pdf_path.rb
+++ pdf_path.rb
@@ -1,20 +1,24 @@
 # 開発環境 macOS Mojave 言語:ruby  gem:poppler
-require 'open-uri'
+#require 'open-uri'
 require 'Poppler'
 
 #file's path
-url='T1801.pdf'
+url='./basedata/T1813.pdf'
 
 #read pdf files
 reader= Poppler::Document.new(url)
 reader_rm=reader.first.get_text.gsub("
 "," ")
-data=reader_rm.split(" ")
-filename=data[2].delete("(").delete(")")+".csv"
-
+reader_cm=reader_rm.gsub(" ",",")
+$data=reader_cm.split(",")
+$filename=$data[2].delete("(").delete(")")+".csv"
+#puts reader_cm
 #output read data
-File.open(filename,'w') do |csv| # output to csv file
-    data.each do |bo|
-        csv << bo
-    end        
+def output
+    File.open($filename,'w') do |csv| # output to csv file
+        $data.each do |bo|
+            csv << bo+","
+        end        
+    end
 end
+output
